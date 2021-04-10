7d147234fb44b4411f4552779c9d7a9ea7c38b99
Preethi-2824 <81350671+Preethi-2824@users.noreply.github.com>
2021-04-10 11:03:23 +0530

    Create JenkinGit.java

---
+++ JenkinGit.java
@@ -0,0 +1,7 @@
+class JenkinGit{
+Public static void main(String[] args){
+  System.out.println("This is Jenkin Git demo project");
+  System.out.println("Learn automation online youtube series");
+ }
+ }
+ 
