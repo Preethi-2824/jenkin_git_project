4b65ab0e4dcbbd5af59f937a75ca235e3bb91539
Preethi-2824 <81350671+Preethi-2824@users.noreply.github.com>
2021-04-10 11:17:11 +0530

    Update JenkinGit.java

--- JenkinGit.java
+++ JenkinGit.java
@@ -2,6 +2,7 @@ class JenkinGit{
 Public static void main(String[] args){
   System.out.println("This is Jenkin Git demo project");
   System.out.println("Learn automation online youtube series");
+  System.out.println("Hello Everyone");
  }
  }
  
